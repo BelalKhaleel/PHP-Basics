c89a10b7fe9e5abc2c88ffbce6959e91b68c9d49
Belal Khaleel <belalkhaleel19@gmail.com>
2023-02-19 17:32:51 +0200

    step 3

--- php-basics-01/Step_2.php
+++ php-basics-01/Step_2.php
@@ -1,6 +1,6 @@
 "Hello world, Help me I am using PHP"
 <?php
-echo "I am getting errors but I can't see the errors";
+echo "I am getting errors but I can't see the errors\n";
 ?>
 <?php
 print "Search on how to enable PHP Errors";
---
+++ php-basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Belal";
+$email = "belalkhaleel19@gmail.com";
+echo "My name is $name and my email is $email.\n";
+$height = 6;
+$width = 5;
+$area = $height * $width;
+echo $area."\n";
+?>
