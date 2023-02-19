0fd29547c2424f3648e47a12788b24344001bccf
Belal Khaleel <belalkhaleel19@gmail.com>
2023-02-19 20:44:35 +0200

    step 5

---
+++ php-basics-01/Step_5.php
@@ -0,0 +1,10 @@
+<?php
+$course_name = "Javascript";
+$enrolled_students = 42;
+$price = 29.99;
+$on_discount = true;
+echo "Course title: $course_name \n";
+echo "Enrolled Students: $enrolled_students \n";
+echo "Course price: $price $ USD \n";
+echo "Course on discount: $on_discount \n";
+?>
