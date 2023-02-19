ad592d82403c379f40fe43ded01449ae6ad26023
Belal Khaleel <belalkhaleel19@gmail.com>
2023-02-19 18:12:52 +0200

    step 4

---
+++ php-basics-01/Step_4.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Belal";
+$position = "web developer";
+$github_url = "https://github.com/BelalKhaleel";
+echo "Hello, I'm " . $name . ", I'm a " . $position . " please check my github link " . $github_url . "\n";
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url \n";
+echo "Hello, I'm $name",", I'm a $position", " please check my github link $github_url \n";
+?>
