d9f2049c2808ab4815e097418a6f28138a1ef82b
Belal Khaleel <belalkhaleel19@gmail.com>
2023-02-20 01:48:19 +0200

    step 3

---
+++ php-basics-02/Step_3.php
@@ -0,0 +1,22 @@
+<?php
+    //define variable and assign value that you want to swap 
+    $x=5;  
+ 
+    //define variable and assign value that you want to swap
+    $y=10; 
+ 
+    //swap two variable value by using arithmetic operation in PHP
+ 
+    //here you will plus x and y value and assing the value to x
+    $x=$x+$y; 
+ 
+    //here you will minus x to y and assin the value to y
+    $y=$x-$y; 
+ 
+    // here again minus x to y and assing the value to x
+    $x=$x-$y;
+ 
+    //print the swapped value
+    echo "Value of x: $x\n";  
+    echo "Value of y: $y\n";  
+?>  
