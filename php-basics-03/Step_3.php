33a0c80e5b317a30f8ee5917aa49fcad8d170459
Belal Khaleel <belalkhaleel19@gmail.com>
2023-02-20 08:21:05 +0200

    step 4

---
+++ php-basics-03/Step_3.php
@@ -0,0 +1,20 @@
+<!-- Using while loop write a programme that count number of times a word present in a sentence.
+The programme should take as argument the text and the word to be searched for
+Hint use `explode(" ", $text); // explode function looks for " " and creates an array, where each word is an element of the array
+Hint use count function to get the number of iterations/repetitions -->
+<!-- <?php
+$text="The quick brown Fox jumps over the lazy Dog";
+$words = explode(" ", $text); // explode function looks for " " and creates an array, where each word is an element of the array
+$now = count($words);
+$j = 0;
+$i=0;
+while($i<$now)
+{
+$i++;
+}
+if ($words[$i] == "the" or $words[$i] == "The")
+{
+$j =  $j+1;
+}
+echo $j;
+?> -->
---
+++ php-basics-03/Step_4.php
@@ -0,0 +1,13 @@
+<?php
+   function sum_of_digits($my_num){
+      $sum = 0;
+      for ($i = 0; $i < strlen($my_num); $i++){
+         $sum += $my_num[$i];
+      }
+      return $sum;
+   }
+   $my_num = "61925836";
+   print_r("The sum of digits is ");
+   echo sum_of_digits($my_num);
+   echo "\n";
+?>
